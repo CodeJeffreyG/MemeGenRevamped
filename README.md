e8bdfbdb2707bff254e70ecfe2b0f411d88c1cc2
Jeffrey <jeffrey.graham600@gmail.com>
2022-11-25 14:51:12 -0500

    TSx interface meme object

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Redoing original meme gen project with proper typescript typings and proper file
 The purpose is to show the difference in my skill then compared to now.
 
 Started With NavBar created a navBar Componenent and a seperate image holding file. Imported NavBar Into app with decent styling.
+
+See Line 7 on app.tsx created typing interface for meme state to hold memeObject that in the future will display image of meme.
--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import {useState, useEffect} from "react";
 import NavBar from "../NavBar/NavBar";
 import "./App.css";
 
 function App() {
+
+  interface Meme {
+    topText: string,
+    bottomText: string,
+    memeImage: string
+  }
+
+  const [meme, setMeme] = useState<Meme>({
+    topText: "",
+    bottomText: "",
+    memeImage: ""
+  });
+
   return (
     <div className="pageContainerOutterPage">
       <NavBar />
