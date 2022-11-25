2a98d49f690ca5c52a112e7a07854725a7f4b76d
Jeffrey <jeffrey.graham600@gmail.com>
2022-11-25 15:05:12 -0500

    Proper Typing for every keyv pair

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ The purpose is to show the difference in my skill then compared to now.
 Started With NavBar created a navBar Componenent and a seperate image holding file. Imported NavBar Into app with decent styling.
 
 See Line 7 on app.tsx created typing interface for meme state to hold memeObject that in the future will display image of meme.
+
+The image flip API quite literally returns 100 meme Images from image flip API
+
+See Line 21. Grabed The images from imageFlip API set the typing to string | [{}] (string or an array of objects)
--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,20 +1,43 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import NavBar from "../NavBar/NavBar";
 import "./App.css";
 
 function App() {
-
   interface Meme {
-    topText: string,
-    bottomText: string,
-    memeImage: string
+    topText: string;
+    bottomText: string;
+    memeImage: string;
   }
+
+  //memeImages from image flip url is just an array filled with 100 objects holding images.
+  type AllMemes =
+    | string
+    | [
+        {
+          box_Count: number;
+          height: number;
+          id: string;
+          name: string;
+          url: string;
+          width: number;
+        }
+      ];
 
   const [meme, setMeme] = useState<Meme>({
     topText: "",
     bottomText: "",
-    memeImage: ""
+    memeImage: "",
   });
+
+  const [allMemes, setAllMemes] = useState<AllMemes>("");
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data));
+  }, []);
+
+  console.log(allMemes);
 
   return (
     <div className="pageContainerOutterPage">
